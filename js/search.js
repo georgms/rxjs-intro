8240bce4e3184c7b63a91b162deb59d7ebeb58e1
Georg M. Sorst <g.sorst@findologic.com>
2019-04-01 21:25:13 +0200

    Reformat

--- js/search.js
+++ js/search.js
@@ -2,15 +2,15 @@ document.querySelector('#search').addEventListener('input', function (event) {
     let query = event.target.value;
 
     let url = 'proxy.php?action=opensearch&origin=*&time=' + new Date().getTime() + '&search=' + query;
-        fetch(url)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (json) {
-                let titles = json[1];
-                let links = json[3];
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            let titles = json[1];
+            let links = json[3];
 
-                let container = document.querySelector('#output');
-                render(container, titles, links);
-            });
+            let container = document.querySelector('#output');
+            render(container, titles, links);
+        });
 });
