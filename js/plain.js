a092f0887923096969abdfd500b4387d739ffecb
Georg M. Sorst <g.sorst@findologic.com>
2019-04-01 20:07:21 +0200

    Basic autocomplete functionality

---
+++ js/plain.js
@@ -0,0 +1,20 @@
+document.querySelector('#search').addEventListener('input', function (event) {
+    let container = document.querySelector('#output');
+
+    let query = event.target.value;
+    if (query.trim() === '') {
+        container.innerHTML = '';
+    } else {
+
+        let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=' + query;
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                let titles = json[1];
+                let links = json[3];
+                render(container, titles, links);
+            });
+    }
+});
